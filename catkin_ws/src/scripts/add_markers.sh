7bceb24996acfc7c945104f56c4f08ced27e3cfe
Aldo <aldo.orta@eu4m.eu>
2021-10-08 05:17:17 +0000

    Additional node in add_markers package to fulfill requirement of execution based on time only

--- catkin_ws/src/scripts/add_markers.sh
+++ catkin_ws/src/scripts/add_markers.sh
@@ -19,8 +19,8 @@ rosparam set /move_base/local_costmap/inflation_layer/inflation_radius 0.5
 
 sleep 5
 
-xterm  -e "cd ${path_catkin_ws} && source devel/setup.bash && roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+xterm  -e "cd ${path_catkin_ws} && source devel/setup.bash && rosrun rviz rviz -d $(rospack find rvizConfig)/HomeService.rviz" &
 sleep 5
 
-xterm  -e "cd ${path_catkin_ws} && source devel/setup.bash && rosrun add_markers add_markers " &
+xterm  -e "cd ${path_catkin_ws} && source devel/setup.bash && rosrun add_markers add_markers_time " &
 sleep 5
